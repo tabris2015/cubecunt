f9283e48247c4ebbf6b8ff24bdd8391af31f317e
Jose Laruta <eduardo.laruta@gmail.com>
2019-04-14 13:12:28 -0400

    gitignore

---
+++ include/bluebot.h
@@ -0,0 +1,18 @@
+#include <robotcontrol.h>
+
+class BlueBot
+{
+private:
+    /* data */
+public:
+    BlueBot(/* args */);
+    ~BlueBot();
+};
+
+BlueBot::BlueBot(/* args */)
+{
+}
+
+BlueBot::~BlueBot()
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,93 @@
-#include <stdio.h>
-#include <rc/button.h>
-#include <rc/led.h>
-#include <rc/start_stop.h>
-#include <rc/time.h>
+#include <iostream>
+#include <robotcontrol.h>
+
+// function declarations
+void on_pause_press();
+void on_pause_release();
 
 int main(void)
 {
-    printf("hola bola");
+    std::cout << "hola bola" << std::endl;
+
+    if (rc_kill_existing_process(2.0) < -2)
+        return -1;
+    // start signal handler so we can exit cleanly
+    if (rc_enable_signal_handler() == -1)
+    {
+        fprintf(stderr, "ERROR: failed to start signal handler\n");
+        return -1;
+    }
+    // initialize pause button
+    if (rc_button_init(RC_BTN_PIN_PAUSE, RC_BTN_POLARITY_NORM_HIGH,
+                       RC_BTN_DEBOUNCE_DEFAULT_US))
+    {
+        fprintf(stderr, "ERROR: failed to initialize pause button\n");
+        return -1;
+    }
+    // Assign functions to be called when button events occur
+    rc_button_set_callbacks(RC_BTN_PIN_PAUSE, on_pause_press, on_pause_release);
+    // make PID file to indicate your project is running
+    // due to the check made on the call to rc_kill_existing_process() above
+    // we can be fairly confident there is no PID file already and we can
+    // make our own safely.
+    rc_make_pid_file();
+    printf("\nPress and release pause button to turn green LED on and off\n");
+    printf("hold pause button down for 2 seconds to exit\n");
+    // Keep looping until state changes to EXITING
+    rc_set_state(RUNNING);
+    while (rc_get_state() != EXITING)
+    {
+        // do things based on the state
+        if (rc_get_state() == RUNNING)
+        {
+            rc_led_set(RC_LED_GREEN, 1);
+            rc_led_set(RC_LED_RED, 0);
+        }
+        else
+        {
+            rc_led_set(RC_LED_GREEN, 0);
+            rc_led_set(RC_LED_RED, 1);
+        }
+        // always sleep at some point
+        rc_usleep(100000);
+    }
+    // turn off LEDs and close file descriptors
+    rc_led_set(RC_LED_GREEN, 0);
+    rc_led_set(RC_LED_RED, 0);
+    rc_led_cleanup();
+    rc_button_cleanup();  // stop button handlers
+    rc_remove_pid_file(); // remove pid file LAST
+    return 0;
+}
+
+/**
+ * Make the Pause button toggle between paused and running states.
+ */
+void on_pause_release()
+{
+    if (rc_get_state() == RUNNING)
+        rc_set_state(PAUSED);
+    else if (rc_get_state() == PAUSED)
+        rc_set_state(RUNNING);
+    return;
+}
+/**
+* If the user holds the pause button for 2 seconds, set state to EXITING which
+* triggers the rest of the program to exit cleanly.
+**/
+void on_pause_press()
+{
+    int i;
+    const int samples = 100;     // check for release 100 times in this period
+    const int us_wait = 2000000; // 2 seconds
+    // now keep checking to see if the button is still held down
+    for (i = 0; i < samples; i++)
+    {
+        rc_usleep(us_wait / samples);
+        if (rc_button_get_state(RC_BTN_PIN_PAUSE) == RC_BTN_STATE_RELEASED)
+            return;
+    }
+    printf("long press detected, shutting down\n");
+    rc_set_state(EXITING);
+    return;
 }
