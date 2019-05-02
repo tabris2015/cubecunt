94a1aed919160bbd0cdf58bd9354df9ab0d3ecf6
Jose Laruta <eduardo.laruta@gmail.com>
2019-05-02 13:14:56 -0400

    correct t

--- src/bluebot.cpp
+++ src/bluebot.cpp
@@ -175,7 +175,7 @@ void BlueBot::updateStatePeriodic()
         angle_pid_.compute();
 
         auto dist_to_goal = distance(last_x_, last_y_, x_goal_, y_goal_);
-        std::cout << "dist: " << dist_to_goal << "/t";
+        std::cout << "dist: " << dist_to_goal << "\t";
         if(dist_to_goal < 0.04) v_ = 0.0;
         // actuation
         driveUnicycle(v_, w_);
