570336e22372d5579adca8bacf5fa7fa25d62cc5
Jose Laruta <eduardo.laruta@gmail.com>
2019-05-27 01:07:27 -0400

    rls file

---
+++ src/rls_motor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "bluebot.h"
+
+using namespace std::chrono;
+
+// 0.11, 0.15, 0.0001
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
